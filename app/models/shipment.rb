462120e9124920fb033cc61fd01b0b339a9806b8
Paul I <ihnatolia3@gmail.com>
2015-01-20 21:12:33 +0200

    Change shipment fields

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,3 +1,4 @@
 class Shipment < ActiveRecord::Base
   validates :po, presence: true
+  enum status: { unresolved: 0, resolved: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116105244) do
+ActiveRecord::Schema.define(version: 20150120190606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shipments", force: :cascade do |t|
     t.string   "po"
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "start"
+    t.datetime "end"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "company"
+    t.integer  "status",     default: 0
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",                            null: false
+    t.string   "uid",                    default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "email"
+    t.text     "tokens"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "company"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
 end
