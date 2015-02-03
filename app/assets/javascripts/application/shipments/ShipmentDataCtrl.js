6e4c91d8f990689e759682ae1305655123b089d3
Oleg Mac <Oleg@Olegs-MacBook-Pro.local>
2015-02-03 14:38:40 +0200

    fix filtering

--- app/assets/javascripts/application/shipments/ShipmentDataCtrl.js
+++ app/assets/javascripts/application/shipments/ShipmentDataCtrl.js
@@ -2,39 +2,33 @@ var dtracker = angular.module('dtracker');
 
 dtracker.controller('ShipmentDataCtrl', ['$http', '$scope', '$filter', 'ngTableParams', function ($http, $scope, $filter, ngTableParams) {
 
-	$http.get('/api/shipments').
-	success(function(data, status, headers, config) {
-    $scope.shipments = data;
+    $http.get('/api/shipments').
+    success(function(data) {
+        $scope.shipments = data;
 
-    $scope.tableParams = new ngTableParams({
-        page: 1,            // show first page
-        count: 10,          // count per page
-        filter: {
-            company: 'M'       // initial filter
-        },
-        sorting: {
-            company: 'asc'     // initial sorting
-        }
-    }, {
-        total: data.length, // length of data
-        getData: function($defer, params) {
-            // use build-in angular filter
-            var filteredData = params.filter() ?
-                    $filter('filter')(data, params.filter()) :
-                    data;
-            var orderedData = params.sorting() ?
-                    $filter('orderBy')(filteredData, params.orderBy()) :
-                    data;
+        $scope.tableParams = new ngTableParams(
+            {
+                page: 1,            // show first page
+                count: 10,          // count per page
+                filter: {
+                    company: ''       // initial filter
+                }
+            }, {
+                total: data.length, // length of data
 
-            params.total(orderedData.length); // set total for recalc pagination
-            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-        }
+                getData: function($defer, params) {
+                    var orderedData = params.filter() ?
+                           $filter('filter')(data, params.filter()) :
+                           data;
+
+                    $scope.shipments = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+
+                    params.total(orderedData.length); 
+                    $defer.resolve($scope.shipments );
+                }
+            });
     });
 
-	}).
-	error(function(data, status, headers, config) {
-		console.log(data);
-	});
 
 }]);
 
