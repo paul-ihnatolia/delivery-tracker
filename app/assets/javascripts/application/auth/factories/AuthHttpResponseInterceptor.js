66e4a7a5ae4dad5e5fe0967d9b187bb6184e959b
Paul I <ihnatolia3@gmail.com>
2015-02-03 12:21:33 +0200

    Fix message

--- app/assets/javascripts/application/auth/factories/AuthHttpResponseInterceptor.js
+++ app/assets/javascripts/application/auth/factories/AuthHttpResponseInterceptor.js
@@ -1,5 +1,5 @@
 angular.module('dtracker')
-  .factory('authHttpResponseInterceptor',['$q','$location', '$rootScope', 'flash', 
+  .factory('authHttpResponseInterceptor',['$q','$location', '$rootScope', 'flash',
       function($q, $location, $rootScope, flash){
       return {
           response: function(response){
@@ -11,9 +11,12 @@ angular.module('dtracker')
           responseError: function(rejection) {
               if (rejection.status === 401) {
                   console.log("Response Error 401",rejection);
-                  $rootScope.flash = flash;
-                  $rootScope.flash.setMessage("You need to login in order to continue");
-                  $location.path('/auth/sign_in').search('returnTo', $location.path());
+                  // No need to redirect user if he is on auth state
+                  if ($location.path() != '/auth/sign_in') {
+                    $rootScope.flash = flash;
+                    $rootScope.flash.setMessage("You need to login in order to continue");
+                    $location.path('/auth/sign_in').search('returnTo', $location.path());
+                  }
               }
               return $q.reject(rejection);
           }
