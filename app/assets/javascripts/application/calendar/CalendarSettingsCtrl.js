2ffb8f2694cda4f352ee196a4c880d036b59cf77
Paul I <ihnatolia3@gmail.com>
2015-02-24 13:13:27 +0200

    Time slot is working

--- app/assets/javascripts/application/calendar/CalendarSettingsCtrl.js
+++ app/assets/javascripts/application/calendar/CalendarSettingsCtrl.js
@@ -4,27 +4,42 @@
     .controller('CalendarSettingsCtrl', ['$scope', '$http',
       function ($scope, $http) {
       var calendarSettings = this;
-      calendarSettings.slot_duration = '00:40:00';
+      var times = $('.settings').data('slot-duration').split(":");
+      var d = new Date();
+      d.setHours( parseInt(times[0], 10) );
+      d.setMinutes( parseInt(times[1], 10) );
+      d.setSeconds( parseInt(times[2], 10));
+      calendarSettings.slot_duration = d;
+      calendarSettings.message = null;
 
       calendarSettings.setSlotDuration = function () {
         // 01:00:00
         // 60
+        var slot_duration = moment(calendarSettings.slot_duration).format("HH:mm:ss");
         $http.put('/api/calendar_settings',
           { calendar_setting:
             {
-              slot_duration: calendarSettings.slot_duration
+              slot_duration: slot_duration
             }
           })
         .success(function(data, status, headers, config) {
           // Notify about success
           // Update attribute
-          $('.settings').data('slot-duration', calendarSettings.slot_duration);
-          var times = calendarSettings.slot_duration.split(':');
+          $('.settings').data('slot-duration', slot_duration);
+          var times = slot_duration.split(':');
           var minutes = parseInt(times[0], 10) * 60 + parseInt(times[1], 10);
           $('.settings').data('schedule-interval', minutes);
+
+          calendarSettings.message = {
+            content: 'Time slot was updates successfully.',
+            type: 'success'
+          };
         })
         .error(function(data, status, headers, config) {
-          //alert();
+          calendarSettings.message = {
+            content: 'Error happened during update.',
+            type: 'error'
+          };
         });
       };
     }]);
