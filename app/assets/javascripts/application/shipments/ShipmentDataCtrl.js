a88aba9a59b6f99532938b09e375d1cc9cf7f00c
Paul I <ihnatolia3@gmail.com>
2015-02-25 16:10:21 +0200

    Fix ng-table

--- app/assets/javascripts/application/shipments/ShipmentDataCtrl.js
+++ app/assets/javascripts/application/shipments/ShipmentDataCtrl.js
@@ -3,78 +3,55 @@ var dtracker = angular.module('dtracker');
 dtracker.controller('ShipmentDataCtrl', ['$http', '$scope', '$filter', 'ngTableParams', function ($http, $scope, $filter, ngTableParams) {
 	var statistics = this;
     statistics.shipments = [];
-    $scope.date_range = { 
+    statistics.tableParams = new ngTableParams(
+        {
+            page: 1,
+            count: 25,
+            filter: {
+                company: ''
+            },
+            sorting: {
+                user: 'asc',
+                company: 'asc'
+            }
+        },
+        {
+            total: 0,
+
+            getData: function($defer, params) {
+                var new_date = $scope.date_range;
+                var start_date = moment(new_date.startDate).format('YYYY/MM/DD');
+                var end_date = moment(new_date.endDate).format('YYYY/MM/DD');
+                var date_range = start_date + '-' + end_date;
+                
+                var http_service = $http.get('/api/shipments?date_range=' + date_range).success(function(sdata) {
+                    var data = sdata;
+                    var filteredData = params.filter() ?
+                       $filter('filter')(data, params.filter()) :
+                       data;
+                    var orderedData = params.sorting() ?
+                        $filter('orderBy')(filteredData, params.orderBy()) :
+                        filteredData;
+                    statistics.shipments = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                    statistics.tableParams.total(orderedData.length);
+                    $defer.resolve(statistics.shipments);
+                }).error(function (error) {
+                    alert('Some error happened!');
+                });
+            }
+        });
+
+    $scope.date_range = {
         startDate: moment().subtract("days", 1),
         endDate: moment()
     };
 
-    // $http.get('/api/shipments').
-    // success(setTable);
-    var first = true;
+    var preventFirstLoading = true;
     $scope.$watch("date_range", function (new_date) {
-        var start_date = moment(new_date.startDate).format('YYYY/MM/DD');
-        var end_date = moment(new_date.endDate).format('YYYY/MM/DD');
-        var date_range = start_date + '-' + end_date;
-        $http.get('/api/shipments?date_range=' + date_range).success(function(data) {
-            if (first) {
-            first = false;
-            setTable(data);
-            } else {
-               statistics.shipments = data;
-               statistics.tableParams.reload(); 
-            }
-        }).error(function (error) {
-            alert('Some error happened!');
-        });
+        if (preventFirstLoading) {
+            preventFirstLoading = false;
+            return;
+        }
+        statistics.tableParams.reload();
     }, false);
-
-    function setTable (data) {
-        statistics.shipments = data;
-        statistics.tableParams = new ngTableParams(
-            {
-                page: 1,            
-                count: 25,          
-                filter: {
-                    company: ''       
-                },
-                sorting: {
-                    company: 'asc'     
-                }
-            }, 
-            {
-                total: statistics.shipments.length,
-
-                getData: function($defer, params) {
-                    var filteredData = params.filter() ?
-                       $filter('filter')(statistics.shipments, params.filter()) :
-                       statistics.shipments;
-
-                    var orderedData = params.sorting() ?
-                        $filter('orderBy')(filteredData, params.orderBy()) :
-                        statistics.shipments;
-       
-                    statistics.shipments = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-
-                    params.total(orderedData.length); 
-                    $defer.resolve(statistics.shipments );
-                }
-            });
-    }
-
-    $scope.$watch(statistics.tableParams, function (params) {
-                    if (!params)
-                        return;
-                    var filteredData = params.filter() ?
-                       $filter('filter')(data, params.filter()) :
-                       data;
-
-                    var orderedData = params.sorting() ?
-                        $filter('orderBy')(filteredData, params.orderBy()) :
-                        data;
-       
-                    statistics.shipments = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-
-                    params.total(orderedData.length); 
-
-    });
 }]);
