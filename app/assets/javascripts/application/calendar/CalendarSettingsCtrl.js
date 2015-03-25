a1f63949ed03542ea6eff4365d4e09c74009fa0a
VasylRishko <rishko92@gmail.com>
2015-03-25 13:19:31 +0200

    Add alert if time interval eq null.

--- app/assets/javascripts/application/calendar/CalendarSettingsCtrl.js
+++ app/assets/javascripts/application/calendar/CalendarSettingsCtrl.js
@@ -1,8 +1,8 @@
 (function () {
   'use strict';
   angular.module('dtracker')
-    .controller('CalendarSettingsCtrl', ['$scope', '$http',
-      function ($scope, $http) {
+    .controller('CalendarSettingsCtrl', ['$scope', '$http', function ($scope, $http) {
+
       var calendarSettings = this;
       var times = $('.settings').data('slot-duration').split(":");
       var d = new Date();
@@ -13,34 +13,40 @@
       calendarSettings.message = null;
 
       calendarSettings.setSlotDuration = function () {
-        // 01:00:00
-        // 60
-        var slot_duration = moment(calendarSettings.slot_duration).format("HH:mm:ss");
-        $http.put('/api/calendar_settings',
-          { calendar_setting:
-            {
-              slot_duration: slot_duration
-            }
-          })
-        .success(function(data, status, headers, config) {
-          // Notify about success
-          // Update attribute
-          $('.settings').data('slot-duration', slot_duration);
-          var times = slot_duration.split(':');
-          var minutes = parseInt(times[0], 10) * 60 + parseInt(times[1], 10);
-          $('.settings').data('schedule-interval', minutes);
 
+        var slot_duration = moment(calendarSettings.slot_duration).format("HH:mm:ss");
+
+        if (slot_duration == '00:00:00') {
           calendarSettings.message = {
-            content: 'Time slot was updates successfully.',
-            type: 'success'
-          };
-        })
-        .error(function(data, status, headers, config) {
-          calendarSettings.message = {
-            content: 'Error happened during update.',
+            content: 'Please set time interval.',
             type: 'error'
           };
-        });
+        } else {
+          $http.put('/api/calendar_settings',
+            { calendar_setting:
+              {
+                slot_duration: slot_duration
+              }
+            })
+            .success(function() {
+
+              $('.settings').data('slot-duration', slot_duration);
+              var times = slot_duration.split(':');
+              var minutes = parseInt(times[0], 10) * 60 + parseInt(times[1], 10);
+              $('.settings').data('schedule-interval', minutes);
+
+              calendarSettings.message = {
+                content: 'Time slot was updates successfully.',
+                type: 'success'
+              };
+            })
+            .error(function() {
+              calendarSettings.message = {
+               content: 'Error happened during update.',
+               type: 'error'
+              };
+            });
+        }
       };
     }]);
 }());
